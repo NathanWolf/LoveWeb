1e8465812d47319b15f044cc787d55e365d99d5a
Nathan Wolf <nathan@elmakers.com>
2023-12-08 12:21:37 -0800

    Data-driven portrait creation work-in-progress

--- scripts/create_portraits.php
+++ scripts/create_portraits.php
@@ -13,6 +13,15 @@ $portraitFolder = $argv[2];
 $targetWidth = $argv[3];
 $targetHeight = $argv[4];
 
+$characterFile = dirname(__FILE__) . '/../data/characters.json';
+if (!file_exists($characterFile)) {
+    die("Can not find file: $characterFile\n");
+}
+$characters = json_decode(file_get_contents($characterFile), true);
+if (!$characters) {
+    die("Could not parse file: $characterFile\n");
+}
+
 function endsWith($haystack, $needle){
     $length = strlen($needle);
     return (substr($haystack, -$length) === $needle);
@@ -26,8 +35,19 @@ foreach ($iterator as $fileInfo) {
     if (!endsWith($filename, '.png')) continue;
     $base = basename($filename);
     $filename = $fileInfo->getPathname();
+    $info = pathinfo($filename);
+    $characterId = basename($filename, '.' . $info['extension']);
+    if (!isset($characters[$characterId])) {
+        echo "Unknown character: $characterId\n";
+        continue;
+    }
+    $character = $characters[$characterId];
+    if (!isset($character['portrait'])) {
+        echo "Character has no portrait location set: $characterId\n";
+        continue;
+    }
 
-    echo "Processing $base\n";
+    echo "Processing $characterId\n";
     $image = imagecreatefrompng($filename);
     if (!$image) {
         echo " Error loading $filename\n";
@@ -35,13 +55,23 @@ foreach ($iterator as $fileInfo) {
     }
 
     list($width, $height) = getimagesize($filename);
+    list($centerX, $centerY) = $character['portrait'];
+    $centerX = floor($centerX * $width);
+    $centerY = floor($centerY * $height);
+
+    $cropHeight = $centerY * 2;
+    $cropWidth = min($width, $cropHeight);
+    $cropSize = min($cropWidth, $cropHeight);
+    $cropX = $centerX - floor($cropWidth / 2);
+    $cropY = $centerY - floor($cropHeight / 2);
 
     $crop = array(
-        'x' => 0,
-        'y' => 0,
-        'width' => $width,
-        'height' => $width
+        'x' => $cropX,
+        'y' => $cropY,
+        'width' => $cropSize,
+        'height' => $cropSize
     );
+    echo " Crop: " . json_encode($crop) . "\n";
     $cropped = imagecrop($image, $crop);
     $scaled = imagescale($cropped, $targetWidth, $targetHeight);
     imagesavealpha($scaled, true);
