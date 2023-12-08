dc2de7af2a1c862cac3d87f5d645b629def4b3ad
Nathan Wolf <nathan@elmakers.com>
2023-12-07 18:51:08 -0800

    Add button to love game

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
             <div class="tabButton" data-tab="flashCards">Flash Cards</div>
             <div class="tabButton" data-tab="relationships">Relationships</div>
             <div class="tabButton" data-tab="tierLists">Tier Lists</div>
+            <div class="tabButton"><a href="https://games.elmakers.com/love" target="_blank">Play Game &#128279;</a></div>
         </div>
 
         <div class="tab" id="characters">
