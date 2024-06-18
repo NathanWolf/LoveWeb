de7cadd5ab62d15c90f584d1a3d0d3b5970d0d56
Nathan Wolf <nathan@elmakers.com>
2024-06-17 22:12:10 -0700

    Add new characters

--- scripts/insert_characters.php
+++ scripts/insert_characters.php
@@ -14,25 +14,18 @@ $db = new \com\elmakers\love\LoveDatabase();
 $characters = $db->getCharacters();
 
 $characterFolder = $argv[1];
-
-function endsWith($haystack, $needle){
-    $length = strlen($needle);
-    return (substr($haystack, -$length) === $needle);
-}
-
 $iterator = new DirectoryIterator($characterFolder);
 foreach ($iterator as $fileInfo) {
     if ($fileInfo->isDot()) continue;
-    if ($fileInfo->isDir()) continue;
-    $filename = $fileInfo->getFilename();
-    if (!endsWith($filename, '.png')) continue;
-    $base = basename($filename);
-    $filename = $fileInfo->getPathname();
-    $info = pathinfo($filename);
-    $characterId = basename($filename, '.' . $info['extension']);
+    if (!$fileInfo->isDir()) continue;
+    $pathname = $fileInfo->getPathname();
+    if (!file_exists($pathname . '/full.png')) continue;
+    $info = pathinfo($pathname);
+    $characterId = $info['filename'];
     if (!isset($characters[$characterId])) {
         $name = ucfirst($characterId);
-        echo "INSERT INTO persona (id, first_name, portrait) values ('$characterId', '$name', '{\"offset\":[0.5,0.1]}');\n";
+        echo "INSERT INTO persona (id, first_name) values ('$characterId', '$name');\n";
         echo "INSERT INTO persona_tier (persona_id, tier_list_id, tier_id) values ('$characterId', 'renown', 'minor');\n";
+        echo "INSERT INTO persona_image (persona_id, image_id, title, description, metadata, tags) values ('$characterId', 'full', 'Full Body', 'This character\\'s full body image', '{\"center\":[256,256]}', 'full,current');\n";
     }
 }
