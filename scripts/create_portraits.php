a3c82d27aa15963b6e3c00dc0d6b128dbb8a9a6c
Nathan Wolf <nathan@elmakers.com>
2025-02-03 12:22:41 -0800

    Add a parameter to export images updated since a specific date

--- scripts/create_portraits.php
+++ scripts/create_portraits.php
@@ -5,26 +5,32 @@ if (PHP_SAPI !== 'cli') {
 }
 
 if (count($argv) < 3) {
-    die("Usage: create_portraits <character folder> <output folder> [width] [height] [number]\n");
+    die("Usage: create_portraits <character folder> <output folder> [all|YYYY-MM-DD] [width] [height] [number]\n");
 }
 
 require_once '../data/LoveDatabase.class.php';
 
 $db = new \com\elmakers\love\LoveDatabase();
-$limit = count($argv) > 5 ? $argv[5] : 0;
+$limit = count($argv) > 6 ? $argv[6] : 0;
 $characters = $db->getCharacters($limit);
 $characterFolder = $argv[1];
 $outputFolder = $argv[2];
 if (!file_exists($outputFolder)) {
     die("Output folder does not exist: $outputFolder\n");
 }
-$targetWidth = count($argv) > 3 ? $argv[3] : 1024;
-$targetHeight = count($argv) > 4 ? $argv[4] : 1024;
+$since = count($argv) > 3 ? $argv[3] : 'all';
+$targetWidth = count($argv) > 4 ? $argv[4] : 1024;
+$targetHeight = count($argv) > 5 ? $argv[5] : 1024;
 
 foreach ($characters as $character) {
     $characterId = $character['id'];
     if (!isset($character['images']['full']) || !isset($character['images']['portrait'])) {
         echo "Skipping $characterId, no full or portrait image available\n";
+        continue;
+    }
+    $updated = $character['images']['portrait']['updated'];
+    if ($since !== 'all' && $updated < $since) {
+        echo "Skipping $characterId, last update was " . $updated . "\n";
         continue;
     }
     $outputFilename = $outputFolder . '/' . $character['name'] . '.png';
@@ -34,4 +40,6 @@ foreach ($characters as $character) {
     }
     echo "Creating $outputFilename\n";
     $db->createPortrait($character['images']['portrait'], $targetWidth, $targetHeight, $outputFilename);
+    $updated = strtotime($updated);
+    touch($outputFilename, $updated);
 }
