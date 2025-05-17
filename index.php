2dc536391df3713e5d8f2d79abf55cb2b568ddaf
Nathan Wolf <nathan@elmakers.com>
2025-05-16 17:28:09 -0700

    Move intro into separate include file

--- index.php
+++ index.php
@@ -93,52 +93,7 @@
         <div class="tabContainer" id="mainTabContainer">
 
         <div class="tab" id="info">
-            <div class="diviinityLogo">
-            </div>
-            <div class="horizontal">
-                <div>
-                    Hello, I’m Cupcakeebug, or Bug for short! I’m an artist,
-                    I digitally draw and animate in my free time, and I do writing as well.
-                    <br/>
-                    I’ve been working on Diviinity since August 3rd, 2023.
-                    <br/>
-                    I use Krita for my drawing, and Flipaclip for my animation.
-                    I use Google docs and Pages for my writing!
-                </div>
-            </div>
-            <div class="horizontal">
-                <div>
-                Diviinity is the book I’m writing! This is the official website, about its plot and its characters. Thank you for checking it out!
-                </div>
-                <div>
-                Diviinity is about the Overlord of the four realms, Thirteen Diviine, the most powerful being ever. Thirteen was imprisoned in the clouds for eternity, on an accusation she destroyed the Shadowrealms. Thirteen’s daughter, the Goddess of Death, Celeste Diviine, goes on a mission to free her mother.
-                </div>
-                <div>
-                Each chapter is a different perspective from a characters point of view, starting off with Cymbeline, the Queen of the Shadowrealms. Each character has their own story in their chapters, whether that be them learning a lesson, making up with a family member or friend, or just goofing off. The chapters may not make sense at first, like a book of short stories, but it all comes together in the end.
-                </div>
-            </div>
-            <div class="horizontal contact">
-                <div>
-                    <a href="https://www.instagram.com/cupcakeebug?igsh=ajNyMWdpNjZsZWk1&utm_source=qr" target="_blank" class="contact">
-                    <span class="icon" style="background-image: url('image/logos/instagram.png')"></span>@cupcakeebug
-                    </a>
-                </div>
-                <div>
-                    <a href="https://youtube.com/@cupcakeebug?feature=shared" target="_blank" class="contact">
-                    <span class="icon" style="background-image: url('image/logos/youtube.png')"></span>@cupcakeebug
-                    </a>
-                </div>
-                <div>
-                    <a href="https://discordapp.com/users/691020896588136529" target="_blank" class="contact">
-                        <span class="icon" style="background-image: url('image/logos/discord.png')"></span>@cupcakeebug
-                    </a>
-                </div>
-                <div>
-                    <a href="mailto:cupcakeebug@gmail.com" target="_blank" class="contact">
-                        <span class="icon" style="background-image: url('image/logos/email.png')"></span>cupcakeebug@gmail.com
-                    </a>
-                </div>
-            </div>
+            <?php require('tabs/intro.inc.php'); ?>
         </div>
 
         <div class="tab" id="slideshow" style="display: none">
