198d971fa115cc5262074af3094a128a658f337c
Nathan Wolf <nathan@elmakers.com>
2025-05-16 17:49:20 -0700

    Update intro page, part 2

--- tabs/intro.inc.php
+++ tabs/intro.inc.php
@@ -21,15 +21,15 @@
 <div class="infoBlock">
     “How can I find more of your work?”
     <br/>
-    I have a YouTube for my animations and an Instagram for my 2D art pieces, both under the title “@CupcakeeBug” ! I also have a website for more Diviinity related things, Diviinity.com.
+    I have a YouTube for my animations and an Instagram for my 2D art pieces, both under the title “@CupcakeeBug” - please see the links below!
     <br/><br/>
     “Besides Diviinity, what other projects are you working on?”
     <br/>
-    I’m writing a prequel to Diviinity, called Diviine, about Thirteen’s creetors! This book is much more political and serious than Diviinity’s semi-lighthearted vibe. I’m also making the comic I mentioned before, as well as a video game! (I’m busy, I know.) The video game is - you’re a mother of 7 immortal kids, and you have to manage that basically. It’s a work I’m progress!
+    I’m writing a prequel to Diviinity, called Diviine, about Thirteen’s creators! This book is much more political and serious than Diviinity’s semi-lighthearted vibe. I’m also making a comic, as well as a video game! (I’m busy, I know.) The video game is - you’re a mother of 7 immortal kids, and you have to manage that basically. It’s a work I’m progress!
     <br/><br/>
     “How can I contact you?”
     <br/>
-    My FAVORITE way of contact is Email. You can use my email Cupcakeebug@gmail.com to messsge me! But I also accept discord and instagram, both under @cupcakeebug.
+    My FAVORITE way of contact is Email. Please see the links below for all of my contact information!
 </div>
 
 </div>
@@ -40,22 +40,25 @@
 
 <div class="infoBlock">
     Diviinity is the story I’ve been writing since 2023. It started off with my OC, Love. The storyline was COMPLETELY different, and revolved around them entirely.
-
+    <br/><br/>
     It grew into what it is now. Love is still a big part of the story, but the main character is now the Overlord, Thirteen Diviine. Thirteen was imprisoned after destroying one of the four realms, the Shadowrealms. Thirteen’s daughter, Celeste, goes on a mission to free her mother from eternal imprisonment.
-
+    <br/><br/>
     Each chapter is from a different perspective, it starts off with the queen of the Shadowrealms during it’s destruction.
-
+    <br/><br/>
     Especially in the beginning, it could be a bit confusing on how this all lines up and connects together to the overall story. But it does, I promise!
 </div>
 
 <div class="infoBlock">
     “When is Diviinity coming out?”
+    <br/>
     Well, I have no set date. I’m really bad working under deadlines. I’m working on editing and illustrating now, and it’s going faster than I could’ve expected! My estimate is anywhere from late 2025 to 2026. My goal is early 2026 at the latest, but things happen. No promises! But it’ll come out one day.
-
+    <br/><br/>
     “How can I support you?”
+    <br/>
     For now, all I ask is for your feedback and presence! The best you can do for me right now is to be am avid part of my dear community, help out with choices, suggest things, and anything else like that! I love having you here.
-
+    <br/><br/>
     “Why’d you choose to illustrate it?”
+    <br/>
     I wanted to combine my favorite things to do, writing and drawing! Not in a comic way, as it lacks the joy I find in writing. (I am making a comic nonetheless, about other characters from the same universe called Ruiin!)
 </div>
 
